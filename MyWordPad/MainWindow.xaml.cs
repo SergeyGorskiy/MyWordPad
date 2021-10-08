2131cac4e1590fd1f78f5b370f692224b7ff6fb2
Sergey Nazhestkin <9497020@mail.ru>
2021-10-08 21:29:14 +0300

    Добавлена логика работы с текстом, а так же возможность сохранения текста в файл и чтения из файла.

--- MyWordPad/MainWindow.xaml.cs
+++ MyWordPad/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,43 +13,86 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MyWordPad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            SetF1CommandBinding();
         }
-
         private void MouseEnterExitArea(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            statBarText.Text = "Exit the Application";
         }
-
         private void MouseLeaveArea(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            statBarText.Text = "Ready";
         }
-
         private void FileExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
         private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            statBarText.Text = "Show Spelling Suggestions";
         }
-
         private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            string spellingHints = string.Empty;
+            SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+            if (error != null)
+            {
+                foreach (string s in error.Suggestions)
+                {
+                    spellingHints += $"{s}\n";
+                }
+                lblSpellingHints.Content = spellingHints;
+                expanderSpelling.IsExpanded = true;
+            }
         }
-
+        private void SetF1CommandBinding()
+        {
+            CommandBinding helpBinding = new CommandBinding(ApplicationCommands.Help);
+            helpBinding.CanExecute += CanHelpExecute;
+            helpBinding.Executed += HelpExecuted;
+            CommandBindings.Add(helpBinding);
+        }
+        private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Look, it is not that difficult. Just type something!", "Help!");
+        }
+        private void CanHelpExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var openDlg = new OpenFileDialog { Filter = "Text Files |*.txt" };
+            if (true == openDlg.ShowDialog())
+            {
+                string dataFromFile = File.ReadAllText(openDlg.FileName);
+                txtData.Text = dataFromFile;
+            }
+        }
+        private void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void SaveCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var saveDlg = new SaveFileDialog { Filter = "Text Files |*.txt" };
+            if (true == saveDlg.ShowDialog())
+            {
+                File.WriteAllText(saveDlg.FileName, txtData.Text);
+            }
+        }
+        private void SaveCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
     }
 }
